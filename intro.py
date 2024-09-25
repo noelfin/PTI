9553fe3e228c3b51e92c334a930d7e1675b7e02a
noelfin <noelfin874@gmail.com>
2024-09-25 16:43:50 +0700

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,20 @@
+# Deklarasi Variabel
+a = 50
+b = 10
+c = a + b
+# Operasi(Operation) -> tambah (+), kurang (-), bagi (/), kali (*), Modulo (%), Pangkat (**)
+print(a)
+print(b)
+print(c)
+Perkalian = a * b
+Pembagian = a / b
+Pengurangan = a - b
+Penambahan = a + b
+Modulo = a % b
+Pangkat = a ** b
+print(Perkalian)
+print(Pembagian)
+print(Pengurangan)
+print(Penambahan)
+print(Modulo)
+print(Pangkat)
